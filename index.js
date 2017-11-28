24226b2ba7640d0f784e387026f48b5191f5a879
Felix Spöttel <hi@felics.me>
2017-11-28 16:41:42 +0100

    Move route-definitions to lib

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const oAuth2Factory = require('./lib/oAuth/factory')
 const session = require('./lib/middlewares/session')
-const { LABELS } = require('./lib/oAuth/config')
+const { makeAuthRoute, makeHomeRoute, makeCallbackRoute } = require('./lib/routes')
 
 const app = express()
 
 app.use(express.static('public'))
-
 app.use(session)
 
 const hbs = exphbs.create({
@@ -22,37 +21,9 @@ app.set('view engine', '.hbs')
 
 const oAuth2 = oAuth2Factory()
 
-app.get('/', (req, res) => {
-  const providerLabel = LABELS[oAuth2.getProvider()]
-
-  res.render('index', {
-    title: `Netlify ${providerLabel} Authorization`,
-    providerLabel
-  })
-})
-
-app.get('/auth', (req, res) => {
-  const { session } = req
-  res.redirect(oAuth2.authorizeUrl(session.id))
-})
-
-app.get('/callback', (req, res) => {
-  const { query, session } = req
-
-  const renderCallback = ({ content, message }) => {
-    const provider = oAuth2.getProvider()
-    session.destroy()
-
-    res.render('callback', {
-      content: JSON.stringify(content),
-      message,
-      provider,
-      title: `${LABELS[provider]} OAuth Callback`
-    })
-  }
-
-  return oAuth2.accessToken(session.id, query).then(renderCallback)
-})
+app.get('/', makeHomeRoute(oAuth2))
+app.get('/auth', makeAuthRoute(oAuth2))
+app.get('/callback', makeCallbackRoute(oAuth2))
 
 const port = process.env.PORT || 3000
 app.listen(port, () => { console.log(`Running on ${port}`) })
